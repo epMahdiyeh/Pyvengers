d3eebe04167abbd5b0a7a8e4f7668c667e0fcae9
Erfan Mahram <erfanmahram@gmail.com>
2023-09-12 00:03:59 +0330

    #6 Create Order urls

---
+++ pineapple/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+app_name = "pineapple"
+
+urlpatterns = [
+    path('order-list/', views.order_list_view, name='order-list'),
+    path('order-detail/<int:pk>', views.order_detail_view, name='order-detail'),
+    path('order-create/', views.order_create_view, name='order-create'),
+    path('order-update/<int:pk>', views.order_update_view, name='order-update'),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
