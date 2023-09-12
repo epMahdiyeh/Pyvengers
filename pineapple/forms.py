943dbeb140771c8fcc6248de589dc9e49e8e0b73
Erfan Mahram <erfanmahram@gmail.com>
2023-09-12 11:46:23 +0330

    #6 OrderForm successfully added and tested on quera

--- pineapple/forms.py
+++ pineapple/forms.py
@@ -1,16 +1,32 @@
+from django import forms
+from django.core.validators import MaxValueValidator
+from .models import Order, Pineapple
 
 
 class SellerForm:
     pass
 
+
 class PineappleForm:
     pass
 
-class OrderForm:
-    pass
+
+class OrderForm(forms.ModelForm):
+    pineapple = forms.ModelChoiceField(queryset=Pineapple.objects.all())
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+    name = forms.CharField(max_length=50)
+    weight_kg = forms.FloatField(validators=[
+        MaxValueValidator(100, '۱۰۰ کیلو آناناس میخوای چیکار؟ مشکل داری؟')
+    ])
+
 
 class SubscriptionForm:
     pass
+
 
 class CommentForm:
     pass
