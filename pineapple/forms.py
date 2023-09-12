948fd25f09ae7c248c1c438f76200ed29532489f
semjz <smalakjan@yahoo.com>
2023-09-13 02:01:21 +0330

    #5 feat: Create seller templates

--- pineapple/forms.py
+++ pineapple/forms.py
@@ -9,7 +9,7 @@ class SellerForm(forms.ModelForm):
         fields = '__all__'
 
     def clean_address(self):
-        # apply address model validation
+        # apply model validation for address
         address = self.cleaned_data["address"]
         # add custom validation
         if len(address) < 10:
@@ -17,7 +17,7 @@ class SellerForm(forms.ModelForm):
         return address
 
     def clean_certificate_code(self):
-        # apply address model validation
+        # apply model validation for certificate_code
         clean_certificate = self.cleaned_data["certificate_code"]
         # add custom validation
         if not clean_certificate.isupper():
